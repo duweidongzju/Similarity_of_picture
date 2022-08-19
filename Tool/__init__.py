4ed00e7533aba0db3ca740133ba178a6a54c19c4
duweidong <3170100800@zju.edu.cn>
2022-08-19 20:36:41 +0800

    add

---
+++ Tool/__init__.py
@@ -0,0 +1,9 @@
+#!C:\pythonCode
+# -*- coding: utf-8 -*-
+# @Time : 2022/8/17 16:22
+# @Author : duweidong
+# @File : __init__.py.py
+# @Software: PyCharm
+
+__all__ = ['read_txt', 'write_txt', 'create_same_dir', 'extract_frame', 'read_excel']
+from Tool.utils_duweidong import *
